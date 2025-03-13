1c93894c81591dda3ae04a26349e76b51b993ca0
Jon Nyman <nymanjon@gmail.com>
2025-03-13 15:17:43 -0700

    Add types to event system.

--- src/react-timer/src/shared/messaging.ts
+++ src/react-timer/src/shared/messaging.ts
@@ -1,10 +1,24 @@
 import { useEffect } from "react"
 
-export function publish(name: string, data: any = null) {
-  document.dispatchEvent(new CustomEvent(name, { detail: data }))
+interface Message {
+  clockAlarm: number
+  clockRestarted: number
+  clockStarted: number
+  clockStopped: number
+  deleteTimer: number
+  dialogClosed: null
+  newTimer: null
 }
 
-export function subscribe(name: string, callback: (data: any) => void, change: any[] | undefined, cleanUp: (() => void) | null = null) {
+export function publish<K extends keyof Message>(name: K, data: Message[K] | null = null) {
+  document.dispatchEvent(new CustomEvent(name as string, { detail: data }))
+}
+
+export function subscribe<K extends keyof Message>(
+  name: K,
+  callback: (data: Message[K]) => void,
+  change: any[] | undefined,
+  cleanUp: (() => void) | null = null) {
   let handler = (e: CustomEvent) => callback(e.detail)
   useEffect(() => {
     // @ts-ignore
