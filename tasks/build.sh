ad7aa90674fd30af456e2a992528a0780f11c767
Jon Nyman <nymanjon@gmail.com>
2025-06-03 22:14:05 -0700

    Make build easier to view by putting it in the bash file.

--- tasks/build.sh
+++ tasks/build.sh
@@ -2,10 +2,13 @@ rm -rf dist
 
 mkdir dist
 
-cp -r static/* dist/
+(cp -r static/* dist/) &
 
-echo "RUN BUILD"
-npm run build
+(npx esbuild src/app-stage-left.ts src/app-vanilla.ts --bundle --format=esm --outdir=dist/js/ --minify) &
+
+(cd src/react-timer/ && npm run build) &
+
+(cd src/van-timer/ && npm run build) &
 
 # in the dist directory loop through the html files and add `?v=<timestamp>` to the end of the css and js files
 TIMESTAMP=$(date +%s)
@@ -15,3 +18,5 @@ for file in dist/*.html; do
     sed -i -E 's/(href=".*\.css)"/\1?v='$TIMESTAMP'"/g' $file
     sed -i -E 's/(src=".*\.js)"/\1?v='$TIMESTAMP'"/g' $file
 done
+
+wait
